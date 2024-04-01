745ac8d79a35c3c51cc785e2b64ffd88df3800fc
azikodev <aziko.coder@gmail.com>
2024-04-01 11:34:31 +0500

    preparing qugo figma with redux

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,47 @@
+import { MousePointer2 } from "lucide-react";
+import "../App.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="container navbar">
+        <div className="cursor flex">
+          <MousePointer2 className="cursor-icon"/>
+          <h3>Москва</h3>
+          <h3>Москва</h3>
+          <h3>Москва</h3>
+        </div>
+        <h3>ООО “Рога и копыта”</h3>
+      </div>
+      <div className="container navbar">
+        <div className="flex">
+        <MousePointer2 className="cursor-icon"/>
+          <h3>Москва</h3>
+          <h3>Москва</h3>
+          <h3>Москва</h3>
+        </div>
+        <h3>ООО “Рога и копыта”</h3>
+      </div>
+      <div className="container navbar">
+        <div className="flex">
+        <MousePointer2 className="cursor-icon"/>
+          <h3>Москва</h3>
+          <h3>Москва</h3>
+          <h3>Москва</h3>
+        </div>
+        <h3>ООО “Рога и копыта”</h3>
+      </div>
+      <div className="container navbar">
+        <div className="flex">
+        <MousePointer2 className="cursor-icon"/>
+          <h3>Москва</h3>
+          <h3>Москва</h3>
+          <h3>Москва</h3>
+        </div>
+        <h3>ООО “Рога и копыта”</h3>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
